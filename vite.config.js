68595a25913bd871b3cd768fcf844e6d9c9274be
MudassarA10 <123820684+MudassarA10@users.noreply.github.com>
2024-02-05 19:10:56 +0500

    commit code

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://MudassarA10.github.io/project-1-food-store/",
 });
