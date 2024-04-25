3c60d071d29c23e2ff100afbab46f61c06ddc46a
MudassarA10 <123820684+MudassarA10@users.noreply.github.com>
2024-04-25 12:55:51 +0500

    coomit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://MudassarA10.github.io/project-1-food-store/",
+  base: "https://MudassarA10.github.io/project-1-food-store/Home/",
 });
